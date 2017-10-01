602a88246debcec93fea7496bdfc4efccc38db58
Ömer Turhan <Wakizashi@Omers-MacBook-Pro.local>
2017-10-01 15:29:29 +0300

    Project initialized

---
+++ VDownloader/VDownloader/ViewControllers/BrowserViewController.swift
@@ -0,0 +1,35 @@
+//
+//  BrowserViewController.swift
+//  VDownloader
+//
+//  Created by Ömer Turhan on 1.10.2017.
+//  Copyright © 2017 Ömer Turhan. All rights reserved.
+//
+
+import UIKit
+
+class BrowserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
